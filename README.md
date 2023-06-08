cbf483ed1126b7a0689faf5dc33684355b42393f
Aditya Pande <adityapande@google.com>
2023-06-08 06:55:56 +0000

    updated readme, added cpp node
    Signed-off-by: Aditya Pande <adityapande@google.com>

--- README.md
+++ README.md
@@ -3,63 +3,24 @@
 This repo is meant to serve as a testing rig for benchmarking network performance of the k4a publisher.
 
 ## Usage
-### Python subscriber
+### python publisher + python subscriber
 After sourcing ros2 rolling installation, run :
 
 ```
 ros2 launch launch/pubsub_py.py
 ```
-I got a throughput of around 270 MB/s using the above launch file.
+I got a throughput of around 270 MB/s (approx 15.82 GB/s) using the above launch file.
 
-This should output the following result:
+### python publisher + cpp subscriber
+After sourcing ros2 rolling installation, build the subscriber cpp node :
 ```
-[subscriber.py-2] ------------------
-[subscriber.py-2] For topic /k4a/color/camera_info
-[subscriber.py-2] Latency mean(ms): 2.384
-[subscriber.py-2] Latency minimum(ms): 1.118
-[subscriber.py-2] Latency maximum(ms): 31.657
-[subscriber.py-2] Latency std dev(ms): 4.056
-[subscriber.py-2] Msg size (bytes): 406
-[subscriber.py-2] Throughput (MB/sec): 0.009
-[subscriber.py-2] ------------------
-[subscriber.py-2] [INFO] [1686098955.507461132] [minimal_subscriber]: 
-[subscriber.py-2] ------------------
-[subscriber.py-2] For topic /k4a/depth/camera_info
-[subscriber.py-2] Latency mean(ms): 2.98
-[subscriber.py-2] Latency minimum(ms): 1.395
-[subscriber.py-2] Latency maximum(ms): 31.846
-[subscriber.py-2] Latency std dev(ms): 4.079
-[subscriber.py-2] Msg size (bytes): 406
-[subscriber.py-2] Throughput (MB/sec): 0.009
-[subscriber.py-2] ------------------
-[subscriber.py-2] [INFO] [1686098955.519252154] [minimal_subscriber]: 
-[subscriber.py-2] ------------------
-[subscriber.py-2] For topic /k4a/depth/image
-[subscriber.py-2] Latency mean(ms): 20.698
-[subscriber.py-2] Latency minimum(ms): 4.829
-[subscriber.py-2] Latency maximum(ms): 309.19
-[subscriber.py-2] Latency std dev(ms): 29.585
-[subscriber.py-2] Msg size (bytes): 737381
-[subscriber.py-2] Throughput (MB/sec): 17.521
-[subscriber.py-2] ------------------
-[subscriber.py-2] [INFO] [1686098955.575008506] [minimal_subscriber]: 
-[subscriber.py-2] ------------------
-[subscriber.py-2] For topic /k4a/color/image
-[subscriber.py-2] Latency mean(ms): 16.545
-[subscriber.py-2] Latency minimum(ms): 5.549
-[subscriber.py-2] Latency maximum(ms): 279.369
-[subscriber.py-2] Latency std dev(ms): 27.316
-[subscriber.py-2] Msg size (bytes): 3686501
-[subscriber.py-2] Throughput (MB/sec): 85.765
-[subscriber.py-2] ------------------
-[subscriber.py-2] [INFO] [1686098955.587713500] [minimal_subscriber]: 
-[subscriber.py-2] ------------------
-[subscriber.py-2] For topic /k4a/points
-[subscriber.py-2] Latency mean(ms): 21.45
-[subscriber.py-2] Latency minimum(ms): 15.271
-[subscriber.py-2] Latency maximum(ms): 37.582
-[subscriber.py-2] Latency std dev(ms): 4.71
-[subscriber.py-2] Msg size (bytes): 5898422
-[subscriber.py-2] Throughput (MB/sec): 152.461
-[subscriber.py-2] ------------------
+cd nodes/subscriber_cpp/build
+cmake ..
+make
 ```
+Then use the launch script :
+
+```
+ros2 launch launch/pubsub_cpp.py
+```
+With this, I got a throughput of 287 MB/s (approx 16.81 GB/s).
--- nodes/subscriber_cpp/subscriber.cpp
+++ nodes/subscriber_cpp/subscriber.cpp
@@ -11,7 +11,8 @@
 #include "sensor_msgs/msg/image.hpp"
 #include "sensor_msgs/msg/camera_info.hpp"
 
-int BUFFER_SIZE = 100;
+long unsigned int BUFFER_SIZE = 101;
+double THROUGHPUT = 0;
 
 std::map<std::string, int> msg_sizes {
   {"/k4a/points", 0},
@@ -21,15 +22,7 @@ std::map<std::string, int> msg_sizes {
   {"/k4a/color/camera_info", 0},
 };
 
-std::map<std::string, std::vector<float>> latencies {
-  {"/k4a/points", {}},
-  {"/k4a/depth/image", {}},
-  {"/k4a/color/image", {}},
-  {"/k4a/depth/camera_info", {}},
-  {"/k4a/color/camera_info", {}},
-};
-
-std::map<std::string, std::vector<float>> current_timestamps {
+std::map<std::string, std::vector<double>> current_timestamps {
   {"/k4a/points", {}},
   {"/k4a/depth/image", {}},
   {"/k4a/color/image", {}},
@@ -38,8 +31,39 @@ std::map<std::string, std::vector<float>> current_timestamps {
 };
 
 template <typename T>
-void msg_callback(const T msg, std::string topic)
-{
+void msg_callback(const T msg, std::string topic, rclcpp::Time current_time) {
+
+  if (current_timestamps[topic].size() == BUFFER_SIZE) {
+    return;
+  }
+
+  auto logger = rclcpp::get_logger("subscriber_cpp");
+
+  double current_nsec = current_time.nanoseconds();
+
+  if (current_timestamps[topic].size() < BUFFER_SIZE) {
+    current_timestamps[topic].push_back(current_nsec);
+  }
+
+  if (current_timestamps[topic].size() == BUFFER_SIZE) {
+    int n = current_timestamps[topic].size();
+    double data_flow = msg_sizes[topic] * (n) / (1024 * 1024);
+    double delta_t = (current_timestamps[topic][n-1] - current_timestamps[topic][0])/ 1000000000 ;
+    auto throughput = data_flow/delta_t;
+
+    THROUGHPUT += throughput;
+
+    std::string report =
+      std::string("\n--------------") +
+      std::string("\nFor topic: ") + topic +
+      std::string("\nMsg size (bytes): ") + std::to_string(msg_sizes[topic]) +
+      std::string("\nThroughput (MB/s): ") + std::to_string(throughput) +
+      std::string("\nCumulative throughput (MB/s): ") + std::to_string(THROUGHPUT) +
+      std::string("\n--------------");
+    
+      RCLCPP_INFO(logger, report.c_str());
+  }
+
   // Find the size of the msg
   if (msg_sizes[topic] == 0) {
     rclcpp::Serialization<T> serializer;
@@ -66,31 +90,31 @@ class MinimalSubscriber : public rclcpp::Node
       subscription_pointcloud = this->create_subscription<sensor_msgs::msg::PointCloud2>(
       "/k4a/points", 10,
       [&](sensor_msgs::msg::PointCloud2 msg) {
-        msg_callback<sensor_msgs::msg::PointCloud2>(msg, "/k4a/points");
+        msg_callback<sensor_msgs::msg::PointCloud2>(msg, "/k4a/points", this->get_clock()->now());
       });
 
       subscription_depth = this->create_subscription<sensor_msgs::msg::Image>(
       "/k4a/depth/image", 10,
       [&](sensor_msgs::msg::Image msg) {
-        msg_callback<sensor_msgs::msg::Image>(msg, "/k4a/depth/image");
+        msg_callback<sensor_msgs::msg::Image>(msg, "/k4a/depth/image", this->get_clock()->now());
       });
 
       subscription_color = this->create_subscription<sensor_msgs::msg::Image>(
       "/k4a/color/image", 10,
       [&](sensor_msgs::msg::Image msg) {
-        msg_callback<sensor_msgs::msg::Image>(msg, "/k4a/color/image");
+        msg_callback<sensor_msgs::msg::Image>(msg, "/k4a/color/image", this->get_clock()->now());
       });
 
       subscription_depth_caminfo = this->create_subscription<sensor_msgs::msg::CameraInfo>(
       "/k4a/depth/camera_info", 10,
       [&](sensor_msgs::msg::CameraInfo msg) {
-        msg_callback<sensor_msgs::msg::CameraInfo>(msg, "/k4a/depth/camera_info");
+        msg_callback<sensor_msgs::msg::CameraInfo>(msg, "/k4a/depth/camera_info", this->get_clock()->now());
       });
 
       subscription_color_caminfo = this->create_subscription<sensor_msgs::msg::CameraInfo>(
       "/k4a/color/camera_info", 10,
       [&](sensor_msgs::msg::CameraInfo msg) {
-        msg_callback<sensor_msgs::msg::CameraInfo>(msg, "/k4a/color/camera_info");
+        msg_callback<sensor_msgs::msg::CameraInfo>(msg, "/k4a/color/camera_info", this->get_clock()->now());
       });
     }
 };
